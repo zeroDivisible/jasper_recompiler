4c300d8d44c3d1a14915ac3aec27ace69af52c46
zeroDivisible <zerodivisible@gmail.com>
2013-07-18 14:10:33 +0100

    added changes in to code to make it work

--- src/main/java/io/application/JasperRecompiler.java
+++ src/main/java/io/application/JasperRecompiler.java
@@ -5,6 +5,12 @@ import net.sf.jasperreports.engine.JasperCompileManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  * Simple class wrapping JasperReports jrxml recompiler
  * @author zerodi
@@ -13,9 +19,21 @@ public class JasperRecompiler {
     private static final Logger log = LoggerFactory.getLogger(JasperRecompiler.class);
 
     public static void main(String[] args) {
-        log.debug("working!");
+        if (args.length == 0) {
+            log.error("Please provide one argument, being path to a folder with templates");
+            return;
+        }
+
         JasperRecompiler jasperRecompiler = new JasperRecompiler();
-        jasperRecompiler.recompileFile("path", "dest");
+        String path = args[0];
+        for (File f : jasperRecompiler.findTemplates(path)) {
+            log.debug("trying to compile {}", f.getPath());
+            try {
+                jasperRecompiler.recompileFile(f.getPath(), jasperRecompiler.findOutputPath(f));
+            } catch (Exception e) {
+                log.error("while compiling templates", e);
+            }
+        }
     }
 
     /**
@@ -26,5 +44,41 @@ public class JasperRecompiler {
      */
     public void recompileFile(String path, String destination) throws JRException {
         JasperCompileManager.compileReportToFile(path, destination);
+    }
+
+
+    /**
+     * Finds all .jrxml files in specified directory. <b>NON-RECURSIVE as of now, can be easily changed.</b>
+     * @param directory
+     * @return
+     */
+    public Collection<File> findTemplates(String directory) {
+        Collection<File> files = new ArrayList<>();
+
+        File directoryToSearch = new File(directory);
+        files.addAll(Arrays.asList(directoryToSearch.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".jrxml");
+            }
+        })));
+
+        return files;
+    }
+
+    /**
+     * Will save output file in the same directory in which the input was, just with .jasper extension
+     * @param input
+     * @return
+     */
+    public String findOutputPath(File input) {
+        String outPath = "";
+        outPath = String.format("%s%s%s%s",
+                input.getParentFile().getPath(),
+                File.separator,
+                input.getName().replace(".jrxml", ""),
+                ".jasper");
+
+        return outPath;
     }
 }
